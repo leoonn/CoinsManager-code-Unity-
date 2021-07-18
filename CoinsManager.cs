5d1cb6d0359063079b0b0eedecf58bbb856b7fa1
leoonn <flateia13@hotmail.com>
2021-07-18 14:12:42 -0300

    Initial commit code

---
+++ CoinsManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CoinsManager : MonoBehaviour
+{
+
+    [Header("Reference value coins")]
+    [SerializeField] public int coins;
+    [Header("Refenrence Text coins")]
+    public TMP_Text textCoins;
+
+    private void Awake()
+    {
+        GetCoins();
+    }
+    
+    //call the method for buy items in shop 
+    public void Buy(int price)
+    {
+        coins = coins - price; //calculate value coins - price of item  
+        textCoins.text = coins.ToString(); //convert coins value to string 
+        SetCoins(); //call method set coins 
+    }
+    public void GetCoins()
+    {
+        coins = PlayerPrefs.GetInt("Coins", coins); //get value coins key and put in variable
+        textCoins.text = coins.ToString(); //convert coins value to string 
+    }
+    public void SetCoins()
+    {
+        PlayerPrefs.SetInt("Coins", coins); //set value coins in key and save
+    }
+
+    
+
+}
